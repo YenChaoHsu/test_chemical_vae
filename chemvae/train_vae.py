e5213af340546da1bb5913da658c26ae37fd71da
YenChaoHsu <105480752+YenChaoHsu@users.noreply.github.com>
2022-05-13 15:04:58 +0800

    Update train_vae.py

--- chemvae/train_vae.py
+++ chemvae/train_vae.py
@@ -13,9 +13,11 @@ encoder and decoder portions of the network
 
 import argparse
 import numpy as np
-import tensorflow as tf
-#config = tf.ConfigProto()
-config = tf.compat.v1.ConfigProto()
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+config = tf.ConfigProto()
+#config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.5
 config.gpu_options.allow_growth = True
 import yaml
